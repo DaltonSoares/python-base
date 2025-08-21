700a6d0ae3d5c955bbde9b2faf59a1c85da71c49
Dalton Augusto Soares <daltonsoares0@gmail.com>
2025-08-21 18:04:22 -0300

    Adicionado shebang com o padrao env. Adicionado comentarios iniciais explicando o uso do programa.

--- hello.py
+++ hello.py
@@ -1,1 +1,26 @@
+#!/usr/bin/env python3
+"""Hello World Multi Linguas.
+
+Dependendo da lingua configura no ambiente o programa exibe a mensagem
+correspondente.
+
+Como usar:
+
+Tenha a variavel LANG devidamente configurada ex:
+
+    export LANG=pt_BR
+
+Execução:
+    python hello.py
+    ou
+    ./hello.py
+"""
+__version__ = "0.0.1"
+__author__ = "Dalton Augusto Soares"
+__license__ = "Unlicense"
+
+# Dunder é uma palavra que começa com dois _ e termina com dois _
+
+
+# Este programa imprime Hello World
 print("Hello, World!")
