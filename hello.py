e3e22a9abe8a8d3115d1b0cecae46a79d5f89f85
Dalton Augusto Soares <dalton.soares@einstein.br>
2025-08-26 14:54:43 -0300

    Versão onde o usuário escolhe o idioma através do input

--- hello.py
+++ hello.py
@@ -20,7 +20,15 @@ __version__ = "0.0.1"
 __author__ = "Dalton Augusto Soares"
 __license__ = "Unlicense"
 
+#current_language = "en_US" # padrao snake_case que tudo é minúsculo.  
+current_language = input("Escolha a linguagem:\nen_US \npt_BR \nit_IT\n\n")
+
 msg = "Hello, World!"
+
+if current_language == "pt_BR":
+    msg = "Olá, Mundo!"
+elif current_language == "it_IT":
+    msg = "Ciao, Mondo!"
 
 # Este programa imprime Hello World
 print(msg)
