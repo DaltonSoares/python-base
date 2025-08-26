45e6d519baf174635c7ba9ec5ec6dab3562ecf8f
Dalton Augusto Soares <dalton.soares@einstein.br>
2025-08-26 17:12:24 -0300

    Implementando o uso do modulo os para verificar qual a lingua do sistema operacional.

--- hello.py
+++ hello.py
@@ -20,15 +20,23 @@ __version__ = "0.0.1"
 __author__ = "Dalton Augusto Soares"
 __license__ = "Unlicense"
 
-#current_language = "en_US" # padrao snake_case que tudo é minúsculo.  
-current_language = input("Escolha a linguagem:\nen_US \npt_BR \nit_IT\n\n")
+# importando modulo os que permite interagir com o sistema operacional.
+import os
 
-msg = "Hello, World!"
+#current_language = "en_US" # padrao snake_case que tudo é minúsculo.
+
+
+current_language = os.getenv("LANG", "en_US")[:5]
+
+msg = "Hola, Mundo!"
 
 if current_language == "pt_BR":
     msg = "Olá, Mundo!"
 elif current_language == "it_IT":
     msg = "Ciao, Mondo!"
+elif current_language == "en_US":
+    msg = "Hello, World!"
+
 
 # Este programa imprime Hello World
 print(msg)
