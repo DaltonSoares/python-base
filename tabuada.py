dab7d75bf17dacb7c6fc9180f849135cbed3a82d
Dalton Augusto Soares <daltonsoares0@gmail.com>
2025-09-05 16:04:59 -0300

    Formatando a tabuada

--- tabuada.py
+++ tabuada.py
@@ -3,27 +3,41 @@
 """Imprime a tabuada do 1 ao 10.
 
 
-Tabuada do 1
-1
-2
-3
--
------------
-Tabuada do 2
-2
-4
------------
+---Tabuada do 1---
+
+    1 x 1 = 1
+    2 x 1 = 2
+    3 x 1 = 3
+...
+
+###################
+---Tabuada do 2---
+
+    2 x 1 = 2
+    2 x 2 = 4
+...
+
+###################
 """
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__ = "Dalton"
+
+template = """
+---Tabuada do {numero}---
+         
+{bloco:^18}
+
+###################
+"""
 
 #numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 # range cria uma se
 numeros = list(range(1,11))
 
 # para cada numero em numeros:
-for numero in numeros:
-    print("Tabuado do: ", numero)
-    for outro_numero in numeros:
-        print(numero * outro_numero)
-    print("-----------------")
+for n1 in numeros:
+    bloco = ""
+    for n2 in numeros:
+        resultado = n1 * n2
+        bloco += f"{n1} x {n2} = {resultado}\n"
+    print(template.format(numero=n1, bloco=bloco))
