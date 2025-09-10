37fb7855eaa77b7fb2f3f99531200f23be23bf23
Dalton Augusto Soares <daltonsoares0@gmail.com>
2025-09-10 18:17:09 -0300

    Informacoes sobre dicionario no comments. Criando arquivo cadastro_de_produto.py para estudo.

---
+++ cadastro_de_produto.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""Cadastro de produto"""
+__version__ : "0.1.0"
+
+from pprint import pprint
+
+produto = {
+    "nome" : "Caneta",
+    #lista
+    "cores": ["azul", "branco"], 
+    "preco" : 3.23,
+    #dicionario
+    "dimensao": {
+        "altura": 12.1,
+        "largura": 0.8
+    },
+    "em_estoque" : True,
+    "codigo" : 45678,
+    "codebar" : None,
+}
+
+cliente = {
+    "nome": "Bruno"
+}
+
+compra = {
+    "cliente": cliente,
+    "produto": produto,
+    "quantidade": 3
+}
+
+#pprint(compra)
+
+total_compra = compra["quantidade"] * compra["produto"]["preco"]
+
+print(
+    f"O cliente {compra["cliente"]["nome"]}",
+    f"comprou {compra["quantidade"]} unidades de",
+    f"{compra["produto"]["nome"]}",
+    f"e pagou o total de {total_compra}"
+)
